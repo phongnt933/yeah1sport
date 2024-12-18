58070f34793e284bd0e40edf5bbe7f2ac57b8ef7
phongnt <phongnt933@gmail.com>
2024-12-18 22:23:17 +0700

    fix: env

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,5 +1,5 @@
-export const API_URL = window._env_.API_URL;
+export const API_URL = window._env_.API_URL || "http://localhost:3000/api";
 export const PUBLIC_URL = window._env_.PUBLIC_URL;
-export const CMS_URL = window._env_.CMS_URL;
+export const CMS_URL = window._env_.CMS_URL || "http://localhost:4000";
 export const API_TIMEOUT = 15 * 1000;
 export const PAGE_SIZE = 10;
